946296f0f965f0fcd75f855ce3a610c395cb6b35
Adminus Kontorius <a-kontorius@yandex.ru>
2018-04-09 17:14:42 +0300

    Закончил с 1-й главой.

---
+++ DataType.java
@@ -0,0 +1,16 @@
+class DataType
+{
+	public static void main(String[] args) 
+	{
+		char letter = 'M';
+		String title = "Java in easy steps";
+		int number = 365;
+		float decimal = 98.6f;
+		boolean result = true;
+
+		System.out.println("Буква " + letter);
+		System.out.println("название " + number);
+		System.out.println("Количество дней " + decimal);
+		System.out.println("Ответ " + result);
+	}
+}
